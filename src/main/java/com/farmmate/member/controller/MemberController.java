98502f636fabe8f5647ea834c3fba81a93e10ef4
swandevson <shai4956@gmail.com>
2024-12-12 23:06:26 +0900

    :memo: 회원 관련 API 명세서 작성

--- src/main/java/com/farmmate/member/controller/MemberController.java
+++ src/main/java/com/farmmate/member/controller/MemberController.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.farmmate.member.dto.response.MemberRegisterResponse;
 import com.farmmate.member.service.MemberService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
+@Tag(name = "회원", description = "회원 관련 API")
 public class MemberController {
 	private final MemberService memberService;
 
+	@Operation(summary = "회원 등록", description = "회원 등록 후, 등록된 회원 정보를 반환합니다.")
 	@PostMapping
 	public MemberRegisterResponse register() {
 		return memberService.register();
--- src/main/java/com/farmmate/member/dto/response/MemberRegisterResponse.java
+++ src/main/java/com/farmmate/member/dto/response/MemberRegisterResponse.java
@@ -1,4 +1,9 @@
 package com.farmmate.member.dto.response;
 
-public record MemberRegisterResponse(String memberId) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "회원 등록 응답")
+public record MemberRegisterResponse(
+	@Schema(description = "회원 ID(식별자)", defaultValue = "member_1") String memberId
+) {
 }
