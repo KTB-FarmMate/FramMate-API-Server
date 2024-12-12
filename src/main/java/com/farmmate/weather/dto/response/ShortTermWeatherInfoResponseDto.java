824a9a0c603d7da8ee4efd8a2b7e58b452d1403f
swandevson <shai4956@gmail.com>
2024-12-12 23:35:55 +0900

    :recycle: 단기 예보 조회의 일간 정보의 강수형태, 하늘상태를 코드로 반환하도록 변경

--- src/main/java/com/farmmate/weather/dto/response/ShortTermWeatherInfoResponseDto.java
+++ src/main/java/com/farmmate/weather/dto/response/ShortTermWeatherInfoResponseDto.java
@@ -4,19 +4,18 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-import com.farmmate.external.weatherstation.type.PrecipitationType;
-import com.farmmate.external.weatherstation.type.SkyCondition;
 import com.farmmate.external.weatherstation.vo.DayForecastVO;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "단기 예보 중 일간 정보 응답")
+@Schema(title = "단기 예보 중 일간 정보 응답")
 public record ShortTermWeatherInfoResponseDto(
-	@Schema(description = "예보 날짜", defaultValue = "2021-01-01") LocalDate forecastDate,
-	@Schema(description = "일 최고기온(섭씨)", defaultValue = "30") Double maxTemperature, // TMX - 최고기온(섭씨)
-	@Schema(description = "일 최저기온(섭씨)", defaultValue = "20") Double minTemperature, // TMN - 최저기온(섭씨)
-	@Schema(description = "시간별 정보", defaultValue = "시간별 정보") List<HourForecastInfo> hourForecastInfos    // 해당 일의 시간별 정보
-
+	@Schema(description = "예보 날짜", example = "2021-01-01") LocalDate forecastDate,
+	@Schema(description = "일 최고기온(섭씨)", example = "30") Double maxTemperature,
+	@Schema(description = "일 최저기온(섭씨)", example = "20") Double minTemperature,
+	@Schema(description = "시간별 정보")
+	List<HourForecastInfo> hourForecastInfos
 ) {
 	public static ShortTermWeatherInfoResponseDto from(DayForecastVO vo) {
 		return new ShortTermWeatherInfoResponseDto(
@@ -29,35 +28,35 @@ public record ShortTermWeatherInfoResponseDto(
 		);
 	}
 
-	@Schema(description = "시간별 정보")
+	@Schema(title = "시간별 예보 정보")
 	public record HourForecastInfo(
-		@Schema(description = "예보 시간", defaultValue = "12:00")
-		LocalTime forecastTime,    // 예보 시간
-		@Schema(description = "강수확률(%)", defaultValue = "50")
-		int precipitationProbability,    // PCP - 강수확률(%)
-		@Schema(description = "강수형태(없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4))", defaultValue = "없음")
-		PrecipitationType precipitationType,    // PTY - 강수형태(없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4))
-		@Schema(description = "1시간 강수량(mm)", defaultValue = "0")
-		int precipitationAmount,    // PCP - 1시간 강수량(mm)
-		@Schema(description = "습도(%)", defaultValue = "50")
-		int humidity, // REH - 습도(%)
-		@Schema(description = "1시간 산적설(cm)", defaultValue = "0")
-		int snowAmount, // SNO - 1시간 산적설(cm)
-		@Schema(description = "하늘상태(맑음(1), 구름조금(2), 구름많음(3), 흐림(4))", defaultValue = "맑음")
-		SkyCondition skyCondition, // SKY - 하늘상태(맑음(1), 구름조금(2), 구름많음(3), 흐림(4))
-		@Schema(description = "기온(℃)", defaultValue = "20")
-		int temperature // TMP - 1시간 기온(℃)
+		@Schema(description = "예보 시간", type = "string", format = "time", example = "12:00")
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+		LocalTime forecastTime,
+		@Schema(description = "강수확률(%)", example = "50")
+		int precipitationProbability,
+		@Schema(description = "강수형태 코드(없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4))", example = "0")
+		int precipitationTypeCode,
+		@Schema(description = "1시간 강수량(mm)", example = "0")
+		int precipitationAmount,
+		@Schema(description = "습도(%)", example = "50")
+		int humidity,
+		@Schema(description = "1시간 산적설(cm)", example = "0")
+		int snowAmount,
+		@Schema(description = "하늘상태(맑음(1), 구름조금(2), 구름많음(3), 흐림(4))", example = "1")
+		int skyConditionCode,
+		@Schema(description = "기온(℃)", example = "20")
+		int temperature
 	) {
 		public static HourForecastInfo from(DayForecastVO.HourForecastInfo hourForecastInfo) {
-
 			return new HourForecastInfo(
 				hourForecastInfo.fcstTime(),
 				hourForecastInfo.precipitationProbability(),
-				hourForecastInfo.precipitationType(),
+				Integer.parseInt(hourForecastInfo.precipitationType().getCode()),
 				hourForecastInfo.precipitationAmount(),
 				hourForecastInfo.humidity(),
 				hourForecastInfo.snowAmount(),
-				hourForecastInfo.skyCondition(),
+				Integer.parseInt(hourForecastInfo.skyCondition().getCode()),
 				hourForecastInfo.temperature()
 			);
 		}
