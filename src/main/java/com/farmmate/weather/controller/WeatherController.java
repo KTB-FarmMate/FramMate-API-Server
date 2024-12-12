75e43d099a02c033d46c1491938a1d27e0822ee7
swandevson <shai4956@gmail.com>
2024-12-12 23:14:37 +0900

    :bug: 날씨 API 라우터가 `RequestMapping`으로 선언되어 있던 버그 수정

--- src/main/java/com/farmmate/weather/controller/WeatherController.java
+++ src/main/java/com/farmmate/weather/controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.farmmate.weather.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,13 @@ public class WeatherController {
 	private final WeatherService weatherService;
 
 	@Operation(summary = "현재 날씨 조회", description = "사용자가 선택한 지역의 현재 날씨 정보를 조회합니다.")
-	@RequestMapping("/current")
+	@GetMapping("/current")
 	public CurrentWeatherInfoResponseDto getTodayWeatherInfo(@NotNull String address) {
 		return weatherService.getCurrentWeatherInfo(address);
 	}
 
 	@Operation(summary = "단기 예보(3일) 조회", description = "사용자가 선택한 지역의 단기 예보(3일) 정보를 조회합니다.")
-	@RequestMapping("/short-term")
+	@GetMapping("/short-term")
 	public List<ShortTermWeatherInfoResponseDto> getShortTermWeatherInfo(@NotNull String address) {
 		return weatherService.getShortTermWeatherInfo(address);
 	}
