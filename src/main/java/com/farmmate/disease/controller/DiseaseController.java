71cb32caa45097348ea9374ce8b0ffab400ff762
swandevson <shai4956@gmail.com>
2024-12-12 23:04:56 +0900

    :memo: 작물 질병정보 상세조회 API 명세서 작성

--- src/main/java/com/farmmate/disease/controller/DiseaseController.java
+++ src/main/java/com/farmmate/disease/controller/DiseaseController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.farmmate.disease.dto.response.DiseaseDetailResponse;
 import com.farmmate.disease.service.DiseaseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "병충해", description = "병충해 정보 관련 API")
 public class DiseaseController {
 	private final DiseaseService diseaseService;
 
+	@Operation(summary = "병충해 상세 조회", description = "사용자가 선택한 작물에 대한 병충해 상세 정보를 조회합니다.")
 	@GetMapping("/diseases")
 	public DiseaseDetailResponse findPests(@RequestParam String cropName, @RequestParam String searchName) {
 		return diseaseService.findPests(cropName, searchName);
--- src/main/java/com/farmmate/disease/dto/response/DiseaseDetailResponse.java
+++ src/main/java/com/farmmate/disease/dto/response/DiseaseDetailResponse.java
@@ -4,23 +4,26 @@ import java.util.List;
 
 import com.farmmate.external.ncpms.vo.DiseaseDetailVo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "병해충 상세정보 응답")
 public record DiseaseDetailResponse(
-	String chemicalPrvnbeMth, // 화학적 방제 방법
-	String cropName,                   // 작물명
-	String sickNameChn,             // 병해 이름(중국어)
-	String preventionMethod,   // 예방 방법
-	List<String> virusImgList,     // 바이러스 이미지 리스트
-	String sickNameKor,             // 병해 이름(한글)
-	String developmentCondition, // 발달 조건
-	String symptoms,                   // 증상
-	String etc,                             // 기타
-	List<String> virusList,           // 바이러스 리스트
-	List<ImageDetail> imageList,      // 이미지 상세 정보 리스트
-	String sickNameEng,             // 병해 이름(영어)
-	String biologyPrvnbeMth    // 생물학적 방제 방법
+	@Schema(description = "화학적 방제 방법", defaultValue = "화학적 방제 방법") String chemicalPrvnbeMth, // 화학적 방제 방법
+	@Schema(description = "작물명", defaultValue = "쌀") String cropName,                   // 작물명
+	@Schema(description = "병해 이름(중국어)", defaultValue = "병해 이름(중국어)") String sickNameChn,             // 병해 이름(중국어)
+	@Schema(description = "예방 방법", defaultValue = "예방 방법") String preventionMethod,   // 예방 방법
+	@Schema(description = "바이러스 이미지 리스트", defaultValue = "바이러스 이미지 리스트") List<String> virusImgList,     // 바이러스 이미지 리스트
+	@Schema(description = "병해 이름(한글)", defaultValue = "병해 이름(한글)") String sickNameKor,             // 병해 이름(한글)
+	@Schema(description = "발달 조건", defaultValue = "발달 조건") String developmentCondition, // 발달 조건
+	@Schema(description = "증상", defaultValue = "증상") String symptoms,                   // 증상
+	@Schema(description = "기타", defaultValue = "기타") String etc,                             // 기타
+	@Schema(description = "바이러스 리스트", defaultValue = "바이러스 리스트") List<String> virusList,           // 바이러스 리스트
+	@Schema(description = "이미지 상세 정보 리스트", defaultValue = "이미지 상세 정보 리스트") List<ImageDetail> imageList,
+	// 이미지 상세 정보 리스트
+	@Schema(description = "병해 이름(영어)", defaultValue = "병해 이름(영어)") String sickNameEng,             // 병해 이름(영어)
+	@Schema(description = "생물학적 방제 방법", defaultValue = "생물학적 방제 방법") String biologyPrvnbeMth    // 생물학적 방제 방법
 ) {
 
 	public static DiseaseDetailResponse fromVo(DiseaseDetailVo vo) {
@@ -44,11 +47,12 @@ public record DiseaseDetailResponse(
 			.build();
 	}
 
+	@Schema(description = "이미지 상세 정보")
 	public record ImageDetail(
-		String image,                     // 이미지 URL
-		String iemSpchcknCode,   // 검사 코드
-		String iemSpchcknNm,       // 검사 이름
-		String imageTitle            // 이미지 제목
+		@Schema(description = "이미지 URL", defaultValue = "이미지 URL") String image,                     // 이미지 URL
+		@Schema(description = "검사 코드", defaultValue = "검사 코드") String iemSpchcknCode,   // 검사 코드
+		@Schema(description = "검사 이름", defaultValue = "검사 이름") String iemSpchcknNm,       // 검사 이름
+		@Schema(description = "이미지 제목", defaultValue = "이미지 제목") String imageTitle            // 이미지 제목
 	) {
 
 	}
