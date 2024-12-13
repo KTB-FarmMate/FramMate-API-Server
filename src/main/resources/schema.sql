eb38f6b33b77678309b2809ff8dfa229e1788f00
swandevson <shai4956@gmail.com>
2024-12-13 14:29:47 +0900

    :wrench: DDL의 PK, FK를 테이블내에서 만들도록 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,39 +2,27 @@ CREATE TABLE `member`
 (
     `id`         CHAR(36)    NOT NULL, -- UUID를 CHAR(36)으로 저장
     `created_at` DATETIME(6) NOT NULL, -- createdAt 필드
-    `updated_at` DATETIME(6) NOT NULL  -- updatedAt 필드
+    `updated_at` DATETIME(6) NOT NULL, -- updatedAt 필드
+    PRIMARY KEY (`id`)                 -- 기본키를 테이블 내에서 설정
 );
 
 CREATE TABLE `crop`
 (
-    `id`   INT          NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NULL
+    `id`   INT          NOT NULL AUTO_INCREMENT, -- id는 자동 증가
+    `name` VARCHAR(100) NULL,                    -- crop name
+    PRIMARY KEY (`id`)                           -- 기본키를 테이블 내에서 설정
 );
 
 CREATE TABLE `chat_room`
 (
-    `id`         CHAR(31)     NOT NULL,
-    `member_id`  CHAR(36)     NOT NULL,
-    `crop_id`    INT          NOT NULL,
-    `created_at` DATETIME(6)  NOT NULL,
-    `updated_at` DATETIME(6)  NOT NULL,
-    `planted_at` DATE         NOT NULL,
-    `address`    VARCHAR(255) NULL
+    `id`         CHAR(31)     NOT NULL,                   -- chat_room id는 CHAR(31)로 설정
+    `member_id`  CHAR(36)     NOT NULL,                   -- member_id는 member 테이블의 id를 참조
+    `crop_id`    INT          NOT NULL,                   -- crop_id는 crop 테이블의 id를 참조
+    `created_at` DATETIME(6)  NOT NULL,                   -- created_at 필드
+    `updated_at` DATETIME(6)  NOT NULL,                   -- updated_at 필드
+    `planted_at` DATE         NOT NULL,                   -- planted_at 필드
+    `address`    VARCHAR(255) NULL,                       -- 주소 필드
+    PRIMARY KEY (`id`),                                   -- 기본키를 테이블 내에서 설정
+    FOREIGN KEY (`member_id`) REFERENCES `member` (`id`), -- member 테이블과 외래키 관계
+    FOREIGN KEY (`crop_id`) REFERENCES `crop` (`id`)      -- crop 테이블과 외래키 관계
 );
-
-ALTER TABLE `member`
-    ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (`id`);
-
-ALTER TABLE `crop`
-    ADD CONSTRAINT `PK_CROP` PRIMARY KEY (`id`);
-
-ALTER TABLE `chat_room`
-    ADD CONSTRAINT `PK_CHAT_ROOM` PRIMARY KEY (`id`);
-
-ALTER TABLE `chat_room`
-    ADD CONSTRAINT `FK_member_TO_chat_room_1` FOREIGN KEY (`member_id`)
-        REFERENCES `member` (`id`);
-
-ALTER TABLE `chat_room`
-    ADD CONSTRAINT `FK_crop_TO_chat_room_1` FOREIGN KEY (`crop_id`)
-        REFERENCES `crop` (`id`);
